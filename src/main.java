89b7b646c3c379cc0b22e5025159711291abc136
hcps-pendyals <hcps-pendyals@180-18PENDYALS.hcps.internal>
2016-04-28 10:25:55 -0400

    This should (once the data is in) be able to draw the map. After I get the data + variables, we'll be able the draw.

---
+++ src/main.java
@@ -0,0 +1,31 @@
+
+import edu.princeton.cs.introcs.StdDraw;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hcps-pendyals
+ */
+import edu.princeton.cs.introcs.*; 
+import java.util.*;
+public class main {
+boolean data = true;
+Scanner scan = new Scanner(System.in);
+    private int variable; //this is would be the name and if we could the variable names w/ numbers at the end so state1, state2
+double[] x = {2.0,3.0,4.0,5.0};
+double [] y = {2.0,3.0,4.0,5.0};
+//Try putting all of them into an array and then go through the array
+double[] state = x;
+while(/*This is would go through the each state and draw everything, which is kinda hard because I don't have the data. The filler data does not help much*/){
+  StdDraw.filledPolygon(x/*We would fill this with the actual data*/, y/*We would fill this with the actual data*/);
+  variable++;  
+} 
+  
+ 
+    
+}
